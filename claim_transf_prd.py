58de4138ace42a79a7c6feaef9af6e3cf183e7d5
susmithaswarna111-afk <susmithaswarna111@gmail.com>
2026-06-28 17:57:24 +0000

    Modified the existing logic and added the new logic

--- claim_transf_prd.py
+++ claim_transf_prd.py
@@ -16,10 +16,11 @@ claims_df = drop_columns(claims_df,['_id'])
 
 # COMMAND ----------
 
-claims_df = claims_df.replace("NaN",None)
-claims_df = claims_df.fillna({"Claim_Or_Rejected":"N"})
-claims_df = claims_df.withColumn("claim_amount", claims_df['claim_amount'].cast("integer"))
-claims_df = claims_df.withColumn("claim_date", claims_df['claim_date'].cast("date"))
+claims.select("*".show())
+
+# COMMAND ----------
+
+claims_df.select("*").filter(col("SUB_ID").isin(["SUBID10022","SUBID10049"])).show(5)
 
 # COMMAND ----------
 
